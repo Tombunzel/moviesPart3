5789028a311b6233f06bc07312626e40978851d2
Tombunzel <bunzel.tom@gmail.com>
2024-10-01 23:11:45 +0200

    try different storage files

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from movie_app import MovieApp
 from storage.storage_csv import StorageCsv
 from storage.storage_json import StorageJson
 
-# storage = StorageJson('movies.json')
-# storage = StorageCsv('mary.csv')
-storage = StorageJson('tom.json')
+storage = StorageJson('data.json')
+# storage = StorageCsv('lilach.csv')
+# storage = StorageJson('tom.json')
 movie_app = MovieApp(storage)
 movie_app.run()
