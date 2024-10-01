e95feb6b8c78a39645783e1a41a4c876da3a2f6e
Tombunzel <bunzel.tom@gmail.com>
2024-10-01 20:17:48 +0200

    adjust docstrings and file_path to support new directory

--- storage_json.py
+++ storage_json.py
@@ -3,7 +3,7 @@ import json
 
 
 class StorageJson(IStorage):
-    """Class to handle json storage"""
+    """Class to handle json data"""
     def __init__(self, file_path):
         """constructor"""
         self.file_path = file_path
@@ -12,7 +12,7 @@ class StorageJson(IStorage):
         """
         Gets movies as an argument and saves them to the JSON file.
         """
-        with open(self.file_path, "w", encoding="UTF-8") as fileobj:
+        with open(f'data/{self.file_path}', "w", encoding="UTF-8") as fileobj:
             json_str = json.dumps(new_movies, indent=4)
             fileobj.write(json_str)
 
@@ -20,19 +20,19 @@ class StorageJson(IStorage):
         """gets movies from the json file, if the file doesn't exist, it creates a new one
         returns the movies in the json file"""
         try:
-            with open(self.file_path, "r", encoding="UTF-8") as fileobj:
+            with open(f'data/{self.file_path}', "r", encoding="UTF-8") as fileobj:
                 json_str = fileobj.read()
                 movies = json.loads(json_str)
                 return movies
         except FileNotFoundError:
-            with open(self.file_path, "w", encoding="UTF-8") as fileobj:
+            with open(f'data/{self.file_path}', "w", encoding="UTF-8") as fileobj:
                 movies = '{}'
                 fileobj.write(movies)
                 print(f"\n\033[33mCreated new file '{self.file_path}'\033[00m")
                 return {}
 
     def list_movies(self):
-        """list all movies in storage"""
+        """list all movies in data"""
         movies = self.get_movies()
         for movie, infos in movies.items():
             print(f"{movie} ({infos['year']}): {infos['rating']}")
@@ -47,7 +47,7 @@ class StorageJson(IStorage):
         self.save_movies(movies)
 
     def delete_movie(self, title):
-        """delete movie from storage"""
+        """delete movie from data"""
         movies = self.get_movies()
         del movies[title]
         self.save_movies(movies)
