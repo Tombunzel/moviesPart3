c0cf279df1a081ca91d8d265c73348a85fdee92e
Tombunzel <bunzel.tom@gmail.com>
2024-10-01 20:13:26 +0200

    create data directoy, rename files

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from movie_app import MovieApp
 from storage_json import StorageJson
 from storage_csv import StorageCsv
 
-# storage = StorageJson('storage/movies.json')
-# storage = StorageCsv('storage/mary.csv')
-storage = StorageJson('storage/tom.json')
+# storage = StorageJson('movies.json')
+storage = StorageCsv('mary.csv')
+# storage = StorageJson('tom.json')
 movie_app = MovieApp(storage)
 movie_app.run()
