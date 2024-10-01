42d829d0642ce00aa174ad1237eec75cc278b358
Tombunzel <bunzel.tom@gmail.com>
2024-10-01 18:07:37 +0200

    update file path in main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from movie_app import MovieApp
 from storage_json import StorageJson
 from storage_csv import StorageCsv
 
-# storage = StorageJson('movies.json')
-# storage = StorageCsv('duba.csv')
-storage = StorageJson('storage/shimshon.json')
+# storage = StorageJson('storage/movies.json')
+# storage = StorageCsv('storage/mary.csv')
+storage = StorageJson('storage/tom.json')
 movie_app = MovieApp(storage)
 movie_app.run()
