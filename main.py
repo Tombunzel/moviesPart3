6eeb63ec355ceb3830c8be6dbb55ce521d3f469b
Tombunzel <bunzel.tom@gmail.com>
2024-10-02 10:54:52 +0200

    implement argparse and allow running the script from terminal with the file name as command line argument

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import argparse
+import sys
 from movie_app import MovieApp
 from storage.storage_csv import StorageCsv
 from storage.storage_json import StorageJson
 
-storage = StorageJson('data.json')
-# storage = StorageCsv('lilach.csv')
-# storage = StorageJson('tom.json')
+# Initialize the parser
+parser = argparse.ArgumentParser(description="Run MovieApp with specified storage file.")
+
+# Add argument for the input file
+parser.add_argument('file_name', type=str, help='The input file (must be .json or .csv)')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Extract the file name from the parsed arguments
+file_name = args.file_name
+extension = file_name.split('.')[-1]
+
+# Logic for handling different file extensions
+if extension == 'json':
+    storage = StorageJson(file_name)
+elif extension == 'csv':
+    storage = StorageCsv(file_name)
+else:
+    print("Unsupported file extension")
+    sys.exit()
+
+# Create and run the MovieApp
 movie_app = MovieApp(storage)
 movie_app.run()
