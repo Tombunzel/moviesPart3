24c49baf600c97cac2fdd350f126529ac8ce45f0
Tombunzel <bunzel.tom@gmail.com>
2024-10-12 15:34:41 +0200

    support case insensitivity with new function & PEP8

--- movie_app.py
+++ movie_app.py
@@ -13,8 +13,8 @@ class MovieApp:
 
     def get_list_of_ratings(self):
         """
-        create list of all movie ratings
-        :return: list of all ratings
+        create a list of all movie ratings
+        :return: a list of all ratings
         """
         list_of_ratings = []
         for _, infos in self._storage.get_movies().items():
@@ -44,6 +44,26 @@ class MovieApp:
         print("\033[00m", end='')  # white
         return user_input
 
+    @staticmethod
+    def check_movie_in_data(movie, movies):
+        """
+        checks a user input movie name through the movies in data
+        returns: None if no input was given and prints a message
+                 False if no movie was found and prints a message
+                 The correct movie name from storage if found
+        """
+        if not movie:
+            print("\n\033[31mNothing entered,"
+                  "sending you back to main menu\033[00m")
+            return None
+        movie_titles_lower = {movie.lower(): movie for movie in movies}
+        if movie.lower() not in movie_titles_lower:
+            print(f"\n\033[31mCouldn't find movie {movie}!\033[00m")
+            return False
+
+        correct_movie_title = movie_titles_lower[movie.lower()]
+        return correct_movie_title
+
     def _command_list_movies(self):
         """
         prints all movies in data
@@ -55,23 +75,25 @@ class MovieApp:
 
     def _command_add_movie(self):
         """
-        uses the data_fetcher file to search and fetch a dict of info about a movie
-        from the api, and then writes the info to the json file.
+        uses the data_fetcher file to search and fetch a dict of movie info
+        from the api, and then writes the info to the JSON file.
         """
         while True:
             movie_name = input("\nEnter new movie name: \033[32m")
             if not movie_name:
-                print("\n\033[31mNothing entered, sending you back to main menu\033[00m")
+                print("\n\033[31mNothing entered,"
+                      "sending you back to main menu\033[00m")
                 break
 
             try:
                 movie_dict = data_fetcher.fetch_api_movie_dict(movie_name)
                 if movie_dict['Type'] != 'movie':
-                    print(f"\n\033[31mCouldn't find a movie called {movie_name}\033[00m")
+                    print(f"\n\033[31mCouldn't find a movie called"
+                          f"{movie_name}\033[00m")
                     break
                 title = movie_dict['Title']
                 year = int(movie_dict['Year'])
-                user_input = input(f"\033[00m\nThe following insert was found:"
+                user_input = input(f"\033[00m\nThe following insert was found "
                                    f"'\033[32m{title} ({year})\033[00m'.\n\n"
                                    f"Confirm? (y/n) \033[32m")
                 print('\033[00m', end='')
@@ -79,12 +101,14 @@ class MovieApp:
                 print("\033[31m\nCouldn't find movie\033[00m")
                 continue
             except requests.exceptions.ConnectionError:
-                print("\033[31m\nConnection error, please check your connection.\033[00m")
+                print("\033[31m\nConnection error,"
+                      "please check your connection.\033[00m")
                 break
 
             if user_input.lower() == 'y':
                 if movie_dict['Type'] != 'movie':
-                    print(f"\n\033[31m{title} is not a movie, but a {movie_dict['Type']}\033[00m")
+                    print(f"\n\033[31m{title} is not a movie,"
+                          f"but a {movie_dict['Type']}\033[00m")
                     break
 
                 rating = float(movie_dict['Ratings'][0]['Value'].split('/')[0])
@@ -94,7 +118,8 @@ class MovieApp:
                 imdb_url = imdb_template + movie_imdb_id
                 country = movie_dict['Country'].split(',')[0]
                 flag_urls = data_fetcher.fetch_country_flag_url(country)
-                self._storage.add_movie(title, year, rating, poster_url, imdb_url, flag_urls, user_note='')
+                self._storage.add_movie(title, year, rating, poster_url,
+                                        imdb_url, flag_urls, user_note='')
                 print(f"\033[00m\nMovie {title} successfully added")
                 break
 
@@ -104,48 +129,47 @@ class MovieApp:
                     break
 
             else:
-                print("\033[31m\nSorry, I didn't get that, please try again. \033[00m")
+                print("\033[31m\nSorry, I didn't get that,"
+                      "please try again. \033[00m")
 
     def _command_delete_movie(self):
         """
         removes a movie from data according to user's input
-        :return: -
+        :return:
         """
         movies = self._storage.get_movies()
         while True:
-            name = input("\nEnter movie name to delete: \033[32m")
-            if not name:
-                print("\n\033[31mNothing entered, sending you back to main menu\033[00m")
+            name = input("\033[00mEnter movie name to delete: \033[32m")
+            print("\033[00m", end='')  # white
+            correct_movie_name = self.check_movie_in_data(name, movies)
+            if correct_movie_name is False:
                 break
-            print("\033[00m")  # white
-            if name not in movies:
-                print(f"\033[31mCouldn't find movie {name}\033[00m")
-            else:
-                self._storage.delete_movie(name)
-                print(f"Movie {name} successfully deleted")
-                break
+            if correct_movie_name is None:
+                continue
+            self._storage.delete_movie(correct_movie_name)
+            print(f"Movie {correct_movie_name} successfully deleted")
+            break
 
     def _command_update_movie(self):
         """
         Allows user to insert a note to a movie in data
-        movie_titles_lower is a dict of lowercase titles referring to the correct titles
+        movie_titles_lower is a dict of lowercase titles
+        referring to the correct titles
         """
         movies = self._storage.get_movies()
         while True:
             name = input("\033[00mEnter movie name: \033[32m")
             print("\033[00m", end='')  # white
-            if not name:
-                print("\n\033[31mNothing entered, sending you back to main menu\033[00m")
+            correct_movie_name = self.check_movie_in_data(name, movies)
+            if correct_movie_name is False:
                 break
-            movie_titles_lower = {movie.lower(): movie for movie in movies}
-            if name.lower() not in movie_titles_lower:
-                print(f"\n\033[31mCouldn't find movie {name}!\033[00m")
-                break
-
-            correct_movie_title = movie_titles_lower[name.lower()]
-            user_note = input("Enter movie notes: ")
-            self._storage.update_movie(correct_movie_title, user_note)
-            print(f"\n\033[00mMovie {correct_movie_title} successfully updated")
+            if correct_movie_name is None:
+                continue
+            user_note = input("Enter movie notes: \033[32m")
+            print("\033[00m", end='')  # white
+            self._storage.update_movie(correct_movie_name, user_note)
+            print(f"\n\033[00mMovie {correct_movie_name} "
+                  f"successfully updated")
             break
 
     @staticmethod
@@ -156,25 +180,25 @@ class MovieApp:
         """
         sum_ratings = 0
         amount_of_movies = len(list_of_ratings)
-        for rating in list_of_ratings:  # iterate over all rating values in dict
-            sum_ratings += rating  # increment to sum_ratings
-        average_rating = sum_ratings / amount_of_movies  # calculate average
+        for rating in list_of_ratings:
+            sum_ratings += rating
+        average_rating = sum_ratings / amount_of_movies
         return average_rating
 
     @staticmethod
     def calc_median_rating(movies, list_of_ratings):
         """
-        calculates median rating depending on amount of movies (odd or even)
+        calculates median rating depending on number of movies (odd or even)
         :return: the median rating
         """
         list_of_ratings = sorted(list_of_ratings, reverse=True)
         movies_length = len(movies)
-        if movies_length % 2 == 0:  # if even number of movies
-            left_median = list_of_ratings[int(movies_length / 2) - 1]  # get "left median"
-            right_median = list_of_ratings[int(movies_length / 2)]  # get "right median"
-            median = (left_median + right_median) / 2  # calculate average of both
-            return median
-        median = list_of_ratings[int(len(movies) / 2)]  # get rating in middle of list
+        if movies_length % 2 == 0:
+            left_median = list_of_ratings[int(movies_length / 2) - 1]
+            right_median = list_of_ratings[int(movies_length / 2)]
+            median = (left_median + right_median) / 2
+            return round(median, 1)
+        median = list_of_ratings[int(len(movies) / 2)]
         return round(median, 1)
 
     @staticmethod
@@ -187,12 +211,12 @@ class MovieApp:
         highest_rating = 0
         best_movie = ''
         year = 0
-        for movie, infos in movies.items():  # go over keys and values in dict
+        for movie, infos in movies.items():
             rating = infos['rating']
-            if rating > highest_rating:  # find the next high rating
+            if rating > highest_rating:
                 year = infos['year']
-                highest_rating = rating  # assign rating value
-                best_movie = movie  # assign movie name
+                highest_rating = rating
+                best_movie = movie
 
         return best_movie, year, highest_rating
 
@@ -200,25 +224,25 @@ class MovieApp:
     def get_worst_movie(movies):
         """
         fetches movie with the lowest rating
-        :param movies: dictionary of movies
+        :param movies: movies dictionary
         :return: movie name, its year and its rating
         """
         lowest_rating = 11  # all ratings will be smaller when compared
         worst_movie = ''
         year = 0
-        for movie, infos in movies.items():  # iterate over movies and ratings in dict
+        for movie, infos in movies.items():
             rating = infos['rating']
-            if rating < lowest_rating:  # find next lower rating
+            if rating < lowest_rating:
                 year = infos['year']
-                lowest_rating = rating  # assign rating value
-                worst_movie = movie  # assign movie name
+                lowest_rating = rating
+                worst_movie = movie
 
         return worst_movie, year, lowest_rating
 
     def _command_stats(self):
         """
         use different functions to fetch and print different movies stats
-        :return: -
+        :return:
         """
         movies = self._storage.get_movies()
         if not movies:
@@ -244,30 +268,33 @@ class MovieApp:
         if not movies:
             print("No movies in the database yet")
         else:
-            movie, infos = random.choice(list(movies.items()))  # assign random tuple of movie,rating
+            movie, infos = random.choice(list(movies.items()))
             print(f"\nYour movie for tonight:\n"
                   f"{movie} ({infos['year']}), it's rated: {infos['rating']}")
 
     def _command_search_movie(self):
         """
         allow user to search for a movie in the dictionary
-        :return: -
+        :return:
         """
         movies = self._storage.get_movies()
         while True:
             search_query = input("\nEnter part of movie name: \033[32m")
             if not search_query:
-                print("\n\033[31mNothing entered, sending you back to main menu\033[00m")
+                print("\n\033[31mNothing entered,"
+                      "sending you back to main menu\033[00m")
                 break
             print("\033[00m")  # white
             list_of_matches = difflib.get_close_matches(search_query.lower(),
-                                                        list(movies.keys()), cutoff=0.4)
+                                                        list(movies.keys()),
+                                                        cutoff=0.4)
             if not list_of_matches:
                 print("\033[31mSorry, couldn't find movie.\033[00m")
                 break
 
             for match in list_of_matches:
-                print(f"{match} ({movies[match]['year']}): {movies[match]['rating']}")
+                print(f"{match} ({movies[match]['year']}):"
+                      f"{movies[match]['rating']}")
 
             break
 
@@ -278,16 +305,16 @@ class MovieApp:
         movies = self._storage.get_movies()
         list_of_ratings = self.get_list_of_ratings()
         print()
-        list_of_ratings = sorted(list(set(list_of_ratings)), reverse=True)  # no duplicates, descending
+        list_of_ratings = sorted(list(set(list_of_ratings)), reverse=True)
         for rating_in_list in list_of_ratings:
             for movie, infos in movies.items():
                 rating = infos['rating']
-                if rating_in_list == rating:  # find matching ratings from list in dict
-                    print(f"{movie} ({infos['year']}), {rating}")  # print matching name and rating
+                if rating_in_list == rating:
+                    print(f"{movie} ({infos['year']}), {rating}")
 
     def _command_movies_by_year(self):
         """
-        prints all movies ordered by year of publish, order chosen by user
+        prints all movies ordered by year of publishing, order chosen by user
         """
         movies = self._storage.get_movies()
         list_of_years = []
@@ -302,7 +329,8 @@ class MovieApp:
             if user_input.lower() == "o":
                 is_reverse = False
                 break
-            print("\033[31m\nPlease enter either 'a' for ascending or 'd' for descending\033[00m")
+            print("\033[31m\nPlease enter either 'a' for ascending"
+                  "or 'd' for descending\033[00m")
 
         for info in movies.values():
             list_of_years.append(info['year'])
@@ -325,7 +353,8 @@ class MovieApp:
             user_input = int(user_input)
             return user_input
         except (ValueError, TypeError):
-            print("\033[31m\nPlease enter a number or leave blank for no filter\033[00m\n")
+            print("\033[31m\nPlease enter a number or leave"
+                  "blank for no filter\033[00m\n")
             return user_input
 
     def _command_filter_movies(self):
@@ -334,8 +363,8 @@ class MovieApp:
         """
         movies = self._storage.get_movies()
         while True:
-            minimum_rating = input("\nEnter minimum rating "
-                                   "(leave blank for no minimum rating): \033[32m")
+            minimum_rating = input("\nEnter minimum rating (leave blank for"
+                                   "no minimum rating): \033[32m")
             print("\033[00m", end="")
             if minimum_rating == '':
                 minimum_rating = 0
@@ -343,12 +372,15 @@ class MovieApp:
                 minimum_rating = float(minimum_rating)
                 if 0 <= minimum_rating <= 10:
                     break
-                print("\033[31m\nPlease enter a number between 0 and 10\033[00m")
+                print("\033[31m\nPlease enter a number between"
+                      "0 and 10\033[00m")
             except ValueError:
-                print("\033[31m\nPlease enter a number or leave blank for no filter\033[00m")
+                print("\033[31m\nPlease enter a number or leave"
+                      "blank for no filter\033[00m")
 
         while True:
-            start_year = input("Enter start year (leave blank for no start year): \033[32m")
+            start_year = input("Enter start year (leave blank for"
+                               "no start year): \033[32m")
             print("\033[00m", end="")
             if start_year == '':
                 start_year = 1888
@@ -359,7 +391,8 @@ class MovieApp:
                 break
 
         while True:
-            end_year = input("Enter end year (leave blank for no end year): \033[32m")
+            end_year = input("Enter end year (leave blank for"
+                             "no end year): \033[32m")
             print("\033[00m", end="")
             if end_year == '':
                 end_year = 2024
@@ -370,21 +403,27 @@ class MovieApp:
 
         print("\nFiltered movies:")
         for movie, info in movies.items():
-            if info['rating'] >= minimum_rating and start_year <= info['year'] <= end_year:
+            if (info['rating'] >= minimum_rating and
+                    start_year <= info['year'] <= end_year):
                 print(f"  {movie} ({info['year']}): {info['rating']}")
 
     @staticmethod
     def _read_template():
-        """reads the template html file and returns its contents"""
-        with open("_static/index_template.html", "r", encoding="UTF-8") as handle:
+        """reads the template HTML file and returns its contents"""
+        with open("_static/index_template.html", "r",
+                  encoding="UTF-8") as handle:
             template = handle.read()
             return template
 
     def _create_html(self, template):
-        """creates a new html file using the template and a dict of all movies"""
+        """
+        creates a new HTML file using
+        the template and a dict of all movies
+        """
         movies = self._storage.get_movies()
         site_title = "My Movies Site"
-        with open("_static/index.html", "w", encoding="UTF-8") as handle:
+        with open("_static/index.html", "w",
+                  encoding="UTF-8") as handle:
             output = template
             output = output.replace("__TEMPLATE_TITLE__", site_title)
             for movie in movies.keys():
